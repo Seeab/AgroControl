4045ae1112ae39b7e941728bc7e3df98747002b2
Seeab <sebalejan19@gmail.com>
2025-11-05 14:35:36 -0300

    Correccion Urls

--- AgroControl/urls.py
+++ AgroControl/urls.py
@@ -1,18 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView  # ← AGREGAR ESTA IMPORTACIÓN
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', RedirectView.as_view(pattern_name='cuarteles:lista_cuarteles'), name='home'),
-    path('cuarteles/', include('cuarteles.urls')),
-]
 from django.conf import settings
 from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('autenticacion.urls')),
+    path('', RedirectView.as_view(pattern_name='cuarteles:lista_cuarteles'), name='home'),
+    path('cuarteles/', include('cuarteles.urls')),
     path('inventario/', include('inventario.urls')),
     path('aplicaciones/', include('aplicaciones.urls')),
 ]
